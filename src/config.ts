62581e5fa6baeb81e3069249997d324a39ca1656
w.siener <w.siener@hegla-hanic.de>
2025-05-16 12:55:27 +0200

    add auto reload window, add scm ready delay config - Add functionality to automatically reload window on successful branch switching - Add configuration options for branch switcher: SCM ready delay and auto-reload settings

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+import * as vscode from "vscode";
+
+export function getConfigDefaultBranch(): string {
+    const config = vscode.workspace.getConfiguration("multiRepoBranchSwitcher");
+    return config.get<string>("defaultBranchName", "master");
+}
+
+export function getConfigRegisterChangesDelay(): number {
+    const config = vscode.workspace.getConfiguration("multiRepoBranchSwitcher");
+    return config.get<number>("registerChangesDelay", 1500);
+}
+
+export function getConfigAutoReloadWindow(): string {
+    const config = vscode.workspace.getConfiguration("multiRepoBranchSwitcher");
+    return config.get<string>("autoReloadWindow", "Ask");
+}
